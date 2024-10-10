c1240bfc4e85ec63765650125086d8569980a54f
Ashish Kumar <ashishkg@google.com>
2024-10-10 06:39:39 +0000

    initi

---
+++ service/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	// Define the API endpoint
+	router.HandleFunc("/hello/{name}", sayHello).Methods("GET")
+
+	// Start the server
+	fmt.Println("Server listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// Get the name from the URL parameters
+	params := mux.Vars(r)
+	name := params["name"]
+
+	// Create the greeting message
+	message := fmt.Sprintf("Hello, %s!", name)
+
+	// Write the message to the response
+	fmt.Fprintln(w, message)
+}
